9995ffab3f16aef3e46d2510a20893d74f7dc670
kafinsw <kafinsw@gmail.com>
2026-07-28 14:08:16 +0700

    add assetPrefix /internal

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -5,6 +5,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
   basePath: '/internal',
+  assetPrefix: '/internal',
   allowedDevOrigins: ['192.168.18.28'],
   turbopack: {
     root: __dirname,
